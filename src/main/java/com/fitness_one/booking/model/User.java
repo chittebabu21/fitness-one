b45da4aeb3da2d33fc21a34fa34348d015a0839a
chittebabu <chittebabu@email.com>
2024-11-18 22:15:00 +0800

    Initial commit

---
+++ src/main/java/com/fitness_one/booking/model/User.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.fitness_one.booking.model;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * author: chittebabu
+ */
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+	// properties
+	@Id
+	@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	private Integer userId;
+	
+	@Column(name = "email_address", nullable = false)
+	private String emailAddress;
+	
+	@Column(name = "password_hash", nullable = false)
+	private String passwordHash;
+	
+	@Column(name = "token")
+	private String token;
+	
+	@Column(name = "is_verified", nullable = false)
+	private boolean isVerified;
+	
+	@Column(name = "created_on", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private String createdOn;
+
+
+    // method to format timestamp object
+    @PrePersist
+    private void setCreatedOn() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.createdOn = dateFormat.format(timestamp);
+    }
+	
+}
